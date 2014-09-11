29f3b5366f167b271e600857d08fd3d185fcb487
wulechuan <wulechuan@live.com>
2014-09-11 17:23:36 +0800

    更新了website.js
    新的Chrome浏览器ua字符串中包含了like Mac OS X、iPhone等片段。因此需要额外的判断操作系统平台。尽管这样仍然不够完备。

--- wwwroot/scripts/lly-website.js
+++ wwwroot/scripts/lly-website.js
@@ -61,11 +61,11 @@ function WLCWebsite(wlcJS) {
 		var isWindowsTouch = uaHas('Touch') && isWindows;
 
 		// http://www.enterpriseios.com/wiki/UserAgent
-		var isIpod = uaHas('iPod');
-		var isIphone = uaHas('iPhone');
-		var isIpad = uaHas('iPad');
-		var isIOS = isIpod || isIphone || isIpad || uaHas('like Mac OS X;');
-		var isMacOSX = uaHas('Mac OS X;') && !isIOS;
+		var isIpod = uaHas('iPod') && platformHasNot('Win32');
+		var isIphone = uaHas('iPhone') && platformHasNot('Win32');
+		var isIpad = uaHas('iPad') && platformHasNot('Win32');
+		var isIOS = isIpod || isIphone || isIpad || uaHas('like Mac OS X;') && platformHasNot('Win32');
+		var isMacOSX = uaHas('Mac OS X;') && uaHasNot('like Mac OS X;') && platformHasNot('Win32');
 
 		// isAndroid *NOT* always correct
 		var isAndroid = uaHas('Android') || platformHas('Linux arm');
